ac370c9a02da7fd85c9cf9d8fa5e85bf2eb4ab0b
ksg0617 <kevinsun200206@gmail.com>
2024-02-16 19:41:36 -0500

    progress

--- wisest_data_fetcher/fetcher.py
+++ wisest_data_fetcher/fetcher.py
@@ -1,7 +1,14 @@
 import json
 import requests
 
-def fetch_json(url):
+def fetch_json(task, version='latest'):
+    if task not in ['qa', 'dpr']:
+        raise ValueError("task must be one of 'qa' or 'dpr'")
+    elif task == 'qa':
+        url = 'https://wisest-data.ls3.rnet.torontomu.ca/qa_data/'
+    elif task == 'dpr':
+        url = 'https://wisest-data.ls3.rnet.torontomu.ca/dpr_data/'
+
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
